c9f40a9086e79594554285e3d85a462d1fd14696
Casandra Calderon <angie200896@gmail.com>
2021-09-22 23:28:31 -0400

    Pregunta 1 p1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+  [
+    'Neuer',
+    'Pavard',
+    'Martinez',
+    'Alaba',
+    'Davies',
+    'Kimmich',
+    'Goretzka',
+    'Coman',
+    'Muller',
+    'Gnarby',
+    'Lewandowski',
+  ],
+  [
+    'Burki',
+    'Schulz',
+    'Hummels',
+    'Akanji',
+    'Hakimi',
+    'Weigl',
+    'Witsel',
+    'Hazard',
+    'Brandt',
+    'Sancho',
+    'Gotze',
+  ],
+],
+score: '4:0',
+scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+date: 'Nov 9th, 2037',
+odds: {
+  team1: 1.33,
+  x: 3.25,
+  team2: 6.5,
+  },
+};
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const players1 = [];
+const players2 = [];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+for (let i = 0; i < game.players[0].length; i++) {
+  players1.push(game.players[0][i]);
+};
+for (let j = 0; j < game.players[1].length; j++) {
+  players2.push(game.players[1][j]);
+};
+console.log(players1);
+console.log(players2);
